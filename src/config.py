7c951b610da7eec6523e81fc5761784c65758e70
Severyn <severynson.kurach@gmail.com>
2025-10-07 10:44:14 -0700

    adjusted-for-colab-max-efficiency

--- src/config.py
+++ src/config.py
@@ -39,11 +39,11 @@ CQT_FMIN = 32.7031956626  # C1
 CHECKPOINT_PATH = "checkpoints/crnn_best.pt"
 WINDOW_FRAMES = 50
 HOP_FRAMES = 25
-BATCH_SIZE = 16
+BATCH_SIZE = 16 if os.environ["ENV"] == "local" else 228
 EPOCHS = 15
 LR = 1e-3
 WEIGHT_DECAY = 1e-4
-NUM_WORKERS = 2
+NUM_WORKERS = 2 if os.environ["ENV"] == "local" else 45
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
